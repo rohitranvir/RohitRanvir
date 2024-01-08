de67631d29e9cc330ccec8148991ad7fd5dfe989
rohitranvir <150506132+rohitranvir@users.noreply.github.com>
2024-01-08 17:15:55 +0530

    Add files via upload

---
+++ linking.js
@@ -0,0 +1,207 @@
+// console.log("HEllo world");
+// console.log("Rohit ranvir");
+// let a =5;
+// let b= 15;
+
+// console.log("sum is :", a+b);
+
+
+
+// lectue=re no 3 ; Template literal
+
+
+// let a=10;
+// let b=20;
+// console.log(`your pay is : ${a+b} rupees`);
+// console.log("your pay is :",(a+b),"rupaye");
+
+
+
+// Arithmetic Operator : Lecture no:4;
+   
+
+
+
+// let a = 10;
+// let b = 5;
+// console.log(a+b);
+// console.log(a-b);
+// console.log(a*b);
+// console.log(a/b);
+// console.log(a%b);
+// console.log(a**b);
+// console.log (a++);
+// console.log(++a)
+// console.log(a++)
+
+
+// lecture no5
+
+
+
+// comparasion operator
+
+
+// let age =19;
+// console.log(age!=18);
+// "a"<"B"
+
+
+
+// lecture no 8
+
+
+
+// IF--Statement
+
+
+// console.log("before my if statement")
+// let age=17;
+//  if(age>18){
+//     console.log("you can vote");
+//  }
+//  if(age<18){
+//     console.log("you can not vote")
+//  }
+// console.log("after my if statement")
+
+
+
+// lecture no-9
+
+
+
+// practic qs
+
+
+// let color="green";
+// if(color=="red"){
+//     console.log("Stop! light color is red.");
+// }
+// if(color=="yellow"){
+//     console.log("Slow down.light color is yellow.");
+// }
+// if(color=="green"){
+//     console.log("Go. light color is green.");
+// }
+
+
+// lecture no 10
+
+
+// else if Statement
+
+
+
+// let age= 16;
+// if(age >= 18){
+//     console.log("you can vote");
+// }
+// else if(age < 18){
+//     console.log("you can not vote");
+// }
+//  else if(age >= 15){
+//      console.log("your a child");
+//  }
+
+
+
+// lecture no -- 11
+
+
+
+// else Statement
+
+
+
+// let age =13;
+// if(age>=18){
+//     console.log("you can vote");
+// }
+// else{
+//     console.log("you can not vote");
+// }
+
+
+// lecture no 12
+
+
+// practic qs
+
+// let size ="S";
+// if (size =="XL"){
+//     console.log("price is rs. 250")
+// }
+// else if(size =="L"){
+//     console.log("price is 200");
+// }
+// else if(size =="M"){
+//     console.log("price is 100");
+// }
+// else if(size =="S"){
+//     console.log("price is 50");
+// }
+
+
+
+
+// NESTED IF-ELSE
+
+// lecture no  13
+
+
+
+// let marks =99;
+
+// if(marks>=33){
+//     console.log("Pass");
+//     if(marks>=80){
+//         console.log("Grade = O")
+//     }
+//     else{
+//         console.log("grede = A")
+//     }
+// }
+// else if(marks<33){
+//     console.log("Better luck next time");
+// }
+
+
+
+// lecture no14
+
+// Logical operator
+
+// &&
+ 
+// let mark=90;
+
+// if(mark>=33 && mark>=80){
+//     console.log("pass");
+//     console.log("A+");
+// }
+
+
+// // ||
+
+// let marks =75 ;
+// if(marks>=33 || marks>=80){
+//     console.log("pass");
+//     console.log("A+");
+// }
+
+
+// LECTURE NO 15
+
+
+// Practic Qs  
+
+
+let str ="apple";
+if(str[0]==="a" && str.length > 3){
+    console.log("good string");
+}
+else{
+    console.log("not a good string");
+}
+
